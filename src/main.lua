4702d55e00785692ce4057534e44c9f9b2ab6e68
KoalaGuyy <nathangargoles@yahoo.com>
2026-05-05 08:10:54 +0800

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -12,6 +12,7 @@ function KSLib:GetInfo()
 		builder = "https://raw.githubusercontent.com/KoalaGuyy/Koala-Library/refs/heads/main/src/builder.lua",
 		executorstested = {
 			velocity = 0,
+			delta = 0,
 		}
 		-- for executors tested: val: 0 <- working & tested, 1 <- not working & soon to be supported (for a patch), 2 <- not working & not going to be supported in the same major & minor version.
 	}
@@ -30,7 +31,9 @@ function KSLib:IsReady()
 end
 
 function KSLib:SetBuilder(NewBuilder)
-	Builder = NewBuilder
+	if not KSLib:IsReady() then
+		Builder = NewBuilder
+	end
 	return
 end
 
