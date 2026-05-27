cf9e0e7dbae20744ac24736ec04ef65e2c032c48
KoalaGuyy <koalaspi@yahoo.com>
2026-05-27 19:28:59 +0800

    added more respond codes + support for dropdown and colorpick

--- src/Services/SavingService.lua
+++ src/Services/SavingService.lua
@@ -6,15 +6,21 @@ Service.Version = "ss_v1"
 
 function Service:Save(FileName)
 	local Success, Result = pcall(function()
-		if not Service.ToSave then return end
-		if not Service.KSLib then return end
+		if not Service.ToSave then return "GotNothingToSave" end
+		if not Service.KSLib then return "GotNoKSLib" end
 		
 		local Data = {}
 
 		for i, v in pairs(Service.ToSave) do
 			local Object = Service.KSLib:LookUp(v)
 			if Object and Object.GetValue and typeof(Object.GetValue) == "function" then
-				Data[v] = Object:GetValue()
+				if Object.ObjectType == "ActionDropDown" then
+					Data[v] = {Object:GetName(), Object:GetValue()}
+				elseif Object.ObjectType == "ActionColorPick" then
+					Data[v] = {Object:GetValue().R, Object:GetValue().G, Object:GetValue().B}
+				else
+					Data[v] = Object:GetValue()
+				end
 			end
 		end
 		
@@ -28,12 +34,12 @@ end
 
 function Service:Load(FileName)
 	local Success, Result = pcall(function()
-		if not Service.KSLib then return end
+		if not Service.KSLib then return "GotNoKSLib" end
 		
 		if isfile(FileName) then
 			local Data = game:GetService("HttpService"):JSONDecode(readfile(FileName))
 			
-			if Data.Version ~= Service.Version then return end
+			if Data.Version ~= Service.Version then return "WrongSavingVersion" end
 			
 			if Data.Data then
 				local Debounce = 0
@@ -46,9 +52,15 @@ function Service:Load(FileName)
 					local Object = Service.KSLib:LookUp(i)
 
 					if Object and Object.SetValue and typeof(Object.SetValue) == "function" then
-						Object:SetValue(v)
+						if Object.ObjectType == "ActionDropDown" and v[1] and v[2] then
+							Object:SetValue(v[1], v[2])
+						elseif Object.ObjectType == "ActionColorPick" and v[1] and v[2] and v[3] then
+							Object:SetValue(Color3.new(v[1], v[2], v[3]))
+						else
+							Object:SetValue(v)
+						end
 						if Object.Update and typeof(Object.Update) == "function" then
-							Object:Update(true)
+							Object:Update()
 						end
 					end
 				end
